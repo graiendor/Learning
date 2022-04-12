45a34b9fe05455faf0fee3f6a63c7429e01560c2
Emery Reva <ereva@ob-k4.nsk.21-school.ru>
2022-04-12 23:38:17 +0700

    partial suffix links implemented

--- src/CPP/SuffixTree.cpp
+++ src/CPP/SuffixTree.cpp
@@ -7,4 +7,6 @@
 int main() {
   SuffixTree tree{};
   tree.setString("lenalek");
+
+
 }
--- src/CPP/SuffixTree.h
+++ src/CPP/SuffixTree.h
@@ -17,16 +17,63 @@ class SuffixTree {
   void setString(string input) {
     text = input;
     overallLength = text.length();
+    remainingLength = overallLength;
     construct();
+    cout << root.length;
+    cout << root.children[1].length;
+    cout << root.children[0].end - root.children[0].begin;
+  }
+
+  string test_string() {
+    string res{*root.children[1].begin, *root.children[1].end, *root.children[0].children[0].link->begin, *root.children[0].children[0].link->end};
+    return res;
+  }
+
+  size_t test_root_length() {
+    return root.length;
+  }
+
+  size_t test_child_length() {
+    return root.children[0].length;
+  }
+
+  size_t test_child_child_length() {
+    return root.children[0].children[0].length;
+  }
+
+  bool search(string input) {
+    bool found{false};
+    string::iterator begin {input.begin()}, end{input.end()}, find{text.begin()};
+    size_t position{};
+    while (position < root.length) {
+      find = root.children[position].begin;
+      if (*begin == *find) {
+        found = true;
+        break;
+      }
+      position++;
+    }
+    if (found) {
+      while (begin != end) {
+        if (*begin != *find) {
+          found = false;
+          break;
+        }
+        begin++;
+        find++;
+      }
+    }
+    return found;
   }
 
  private:
   string text{};
   size_t overallLength{};
+  size_t remainingLength{};
 
   struct TreeNode {
     shared_ptr<TreeNode[]> children{};
-    shared_ptr<TreeNode> link{};
+    TreeNode* link{};
     string::iterator begin{};
     string::iterator end{};
     size_t length{};
@@ -39,46 +86,55 @@ class SuffixTree {
   void construct() {
     root.children.reset(new TreeNode[overallLength]);
     root.hasChildren = true;
-    string::iterator begin = text.begin(), end = text.end();
+    string::iterator begin {text.begin()}, end {text.end()};
     size_t edge{};
     for (; begin < end; begin++, edge++) {
       //  phase
       extension(root, edge, begin, end);
+      remainingLength--;
     }
-    cout << *root.children[3].begin << endl;
-    cout << *root.children[3].end << endl;
-
-    cout << *root.children[1].children[1].begin << endl;
-    cout << *root.children[1].children[1].end << endl;
   }
 
   void extension(TreeNode& node, size_t edge, string::iterator& begin, string::iterator& end) {
+    bool needToLink {false};
+    bool isInserted {true};
     for (size_t position = 0; position <= edge; position++) {
       //  extension
       if (node.children[position].isEmpty) {
-
         node.children[position].begin = begin;
         node.children[position].isEmpty = false;
         node.children[position].end = begin;
+        node.children[position].length++;
       } else if (!node.children[position].hasChildren) {
         string::iterator found = checkRepeat(node.children[position].begin, node.children[position].end, *begin);
         if (found != node.children[position].end && found != end) {
           if (*(found + 1) != *(begin + 1) && (found + 1) != end) {
-            cout << *found << " - nashel. " << *node.children[position].end << endl;
-            node.children[position].children.reset(new TreeNode[overallLength - edge]);
-            node.children[position].children[0].end = node.children[position].end;
-            node.children[position].end = found;
-            found++;
-            node.children[position].children[0].begin = found;
-            node.children[position].children[0].isEmpty = false;
-            node.children[position].hasChildren = true;
+            constructNewNode(node.children[position], found);
+            node.children[position].length = node.children[position].end - node.children[position].begin;
+            if (!needToLink) {
+              needToLink = true;
+            } else {
+              node.children[position - 1].children[0].link = &node.children[position].children[0];
+              node.children[position].children[0].link = &node.children[position - 1].children[0];
+              needToLink = false;
+            }
+//            position--;
+            edge--;
+            isInserted = false;
           }
         } else {
           node.children[position].end = begin;
+          node.children[position].length++;
         }
       } else {
-        extension(node.children[position], overallLength - edge, begin, end);
+        isInserted = false;
+        extension(node.children[position], remainingLength, begin, end);
       }
+//      node.children[position].length++;
+//      node.children[position].length = node.children[position].end - node.children[position].begin + 1;
+    }
+    if (isInserted) {
+      node.length++;
     }
   }
 
@@ -87,6 +143,17 @@ class SuffixTree {
       begin++;
     }
     return begin;
+  }
+
+  void constructNewNode(TreeNode& node, string::iterator& found) {
+    node.children.reset(new TreeNode[remainingLength]);
+    node.children[0].length = node.length;
+    node.children[0].end = node.end;
+    node.end = found;
+    found++;
+    node.children[0].begin = found;
+    node.children[0].isEmpty = false;
+    node.hasChildren = true;
   }
 
 
