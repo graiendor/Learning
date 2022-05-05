e7fdb6f7cfc2997b2d8d34917af5e7ec4a785fa4
graiendor <graiendor@gmail.com>
2022-05-05 14:35:34 +0700

    WSGI with cherrypy done

---
+++ src/CommonPython/pyth_04/credentials.py
@@ -0,0 +1,35 @@
+import cherrypy
+import json
+
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+    @cherrypy.expose
+    def credentials(self, specie):
+        base = {"Cyberman": "John Lumic",
+                "Dalek": "Davros",
+                "Judoon": "Shadow Proclamation Convention 15 Enforcer",
+                "Human": "Leonardo da Vinci",
+                "Ood": "Klineman Halpen",
+                "Silence": "Tasha Lem",
+                "Slitheen": "Coca-Cola salesman",
+                "Sontaran": "General Staal",
+                "Time Lord": "Rassilon",
+                "Weeping Angel": "The Division Representative",
+                "Zygon": "Broton"}
+        result = base.get(specie)
+
+        cherrypy.response.status = 404
+        if result != None:
+            cherrypy.response.status = 200
+            result = {specie: result}
+        else:
+            result = {specie: "Unknown"}
+        return json.dumps(result)
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(HelloWorld())
