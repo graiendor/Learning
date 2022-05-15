8ad357cbd244f26f67e163c7cb50788fd6bb9148
ereva <ereva@student.21-school.ru>
2022-05-15 15:17:17 +0700

    alignment, class, length, name implemented / pyth_05

--- src/CommonPython/pyth_05/part_01/python_server.py
+++ src/CommonPython/pyth_05/part_01/python_server.py
@@ -1,8 +1,11 @@
+from enum import IntEnum
+
 import grpc
 from concurrent import futures
 import time
 import server_pb2_grpc as pb2_grpc
 import server_pb2 as pb2
+from random import choice, uniform
 
 
 class ReportService(pb2_grpc.ReportServicer):
@@ -10,11 +13,49 @@ class ReportService(pb2_grpc.ReportServicer):
     def __init__(self):
         pass
 
+
+
+    class Spaceship:
+        class Vessel_class(IntEnum):
+            Corvette = 0
+            Frigate = 1
+            Cruiser = 2
+            Destroyer = 3
+            Carrier = 4
+            Dreadnought = 5
+
+        def __init__(self):
+            self.alignment: int = choice([0, 1])
+            with open('known_officers.txt', 'r') as file:
+                lines = file.read().splitlines()
+                self.name = choice(lines)
+
+            self.vessel_class: int = choice(list(self.Vessel_class))
+            self.length = self.define_length()
+
+        def define_length(self) -> float:
+            length: float = 0
+            if self.vessel_class == self.Vessel_class.Corvette:
+                length = uniform(10, 150)
+            if self.vessel_class == self.Vessel_class.Frigate:
+                length = uniform(30, 250)
+            if self.vessel_class == self.Vessel_class.Cruiser:
+                length = uniform(75, 500)
+            if self.vessel_class == self.Vessel_class.Destroyer:
+                length = uniform(160, 750)
+            if self.vessel_class == self.Vessel_class.Carrier:
+                length = uniform(1000, 1200)
+            if self.vessel_class == self.Vessel_class.Dreadnought:
+                length = uniform(1000, 2500)
+            return length
+
     def GetSpaceship(self, request, context):
         # message = request.message
-        # result = f'Hello I am up and running received "{message}" message from you'
 
-        result = {'alignment': 1, 'name': 'Rachehan', 'length': 450.32, 'class': 3, 'size': 4, 'armed': True,
+        spaceship = self.Spaceship()
+
+        result = {'alignment': spaceship.alignment, 'name': spaceship.name, 'length': spaceship.length,
+                  'class': spaceship.vessel_class, 'size': 4, 'armed': True,
                   'officers': ["Nick", "NeNick"]}
 
         return pb2.SpaceshipInfo(**result)
