45feb5ebb99f076cf0c1a2447eef7f13a9494da6
ereva <ereva@student.21-school.ru>
2022-05-15 21:57:29 +0700

    rGPC client and server implemented | validation with pedantic implemented / pyth_05

--- src/CommonPython/pyth_05/part_01/python_client.py
+++ src/CommonPython/pyth_05/part_01/python_client.py
@@ -1,4 +1,13 @@
-from typing import List
+import json
+from enum import Enum
+
+import google.protobuf.json_format as js
+# import pydantic
+from pydantic import BaseModel, ValidationError, Field, root_validator
+from typing import List, Dict
+from python_server import ReportService
+
+from random import choice
 
 import grpc
 import server_pb2_grpc as pb2_grpc
@@ -6,6 +15,37 @@ import server_pb2 as pb2
 
 
 class ReportClient(object):
+    class SpaceShip(BaseModel):
+        alignment: str
+        name: str
+        length: float
+        vessel_class: str = Field(alias='class')
+        size: int
+        armed: bool
+        officers: List[Dict]
+
+        @root_validator
+        def check(cls, values):
+            index: int = 0
+            for count, value in enumerate(list(ReportService.Spaceship.__Vessel_class__)):
+                if value.name == values["vessel_class"]:
+                    index = count + 1
+                    break
+            length_ranges: list[list[int]] = [[], [80, 250], [300, 600], [500, 1000], [800, 2000], [1000, 4000], [5000, 20000]]
+            size_ranges: list[list[int]] = [[], [4, 10], [10, 15], [15, 30], [50, 80], [120, 250], [300, 500]]
+            armed_ranges: list[bool] = [True, True, True, True, True, False, True]
+            hostile_ranges: list[bool] = [True, True, False, True, False, True, True]
+            if index != 0:
+                if values['length'] not in range(length_ranges[index][0], length_ranges[index][1]):
+                    raise ValueError
+                if values['size'] not in range(size_ranges[index][0], size_ranges[index][1]):
+                    raise ValueError
+                if values['armed'] is not armed_ranges[index]:
+                    raise ValueError
+                if values['alignment'] == 'enemy' and hostile_ranges[index] is False:
+                    raise ValueError
+            return values
+
     def __init__(self):
         self.host = 'localhost'
         self.server_port = 6666
@@ -15,16 +55,38 @@ class ReportClient(object):
             stub = pb2_grpc.ReportStub(channel)
             main_coordinates: List[int] = [24, 54, 30, 32]
             second_coordinates: List[float] = [24.32, 31.24]
-            # message = pb2.Coordinates(coordinates='24 54 24.32 30 32 31.24')
             message = pb2.Coordinates(coordinate1=main_coordinates[0], coordinate2=main_coordinates[1],
                                       coordinate3=second_coordinates[0], coordinate4=main_coordinates[2],
                                       coordinate5=main_coordinates[3], coordinate6=second_coordinates[1])
-            print(f'{message}')
-            response = stub.GetSpaceship(message)
+
+            response = self.generate_json(js.MessageToJson(stub.GetSpaceship(message)))
+            try:
+                self.SpaceShip.parse_raw(response)
+            except ValidationError as error:
+                response = None
+
         return response
+
+    def generate_json(self, response):
+        data = json.loads(response)
+        officers = data.get('officers')
+        if not data.get('armed'):
+            data['armed'] = False
+        if officers:
+            grouped_officers: List[Dict] = []
+            for i in range(0, len(officers), 3):
+                grouped_officers.append(
+                    {'first_name': officers[i], 'last_name': officers[i + 1], 'rank': officers[i + 2]})
+            data['officers'] = grouped_officers
+        else:
+            data['officers'] = []
+        data = json.dumps(data)
+        return data
 
 
 if __name__ == '__main__':
     client = ReportClient()
-    result = client.get_spaceship()
-    print(f'{result}')
+    for _ in range(choice([1, 10])):
+        result = client.get_spaceship()
+        if result is not None:
+            print(f'{result}')
--- src/CommonPython/pyth_05/part_01/python_server.py
+++ src/CommonPython/pyth_05/part_01/python_server.py
@@ -1,11 +1,12 @@
 from enum import IntEnum
+from typing import List, Dict
 
 import grpc
 from concurrent import futures
 import time
 import server_pb2_grpc as pb2_grpc
 import server_pb2 as pb2
-from random import choice, uniform
+from random import choice, uniform, randint
 
 
 class ReportService(pb2_grpc.ReportServicer):
@@ -13,50 +14,79 @@ class ReportService(pb2_grpc.ReportServicer):
     def __init__(self):
         pass
 
-
-
     class Spaceship:
-        class Vessel_class(IntEnum):
-            Corvette = 0
-            Frigate = 1
-            Cruiser = 2
-            Destroyer = 3
-            Carrier = 4
-            Dreadnought = 5
+        class __Vessel_class__(IntEnum):
+            Corvette = 1
+            Frigate = 2
+            Cruiser = 3
+            Destroyer = 4
+            Carrier = 5
+            Dreadnought = 6
+
+        __ranks__: List[str] = ['Commander', 'Lieutenant', 'Warchief', 'Sergeant', 'Chief', 'Captain', 'Colonel', 'Admiral']
 
         def __init__(self):
-            self.alignment: int = choice([0, 1])
-            with open('known_officers.txt', 'r') as file:
+            self.alignment: int = choice([1, 2])
+            with open('spaceship_names.txt', 'r') as file:
                 lines = file.read().splitlines()
-                self.name = choice(lines)
+                self.name: str = choice(lines)
+            self.vessel_class = choice(list(self.__Vessel_class__))
+            if self.alignment == 2:
+                self.vessel_class = choice([self.vessel_class, 7])
 
-            self.vessel_class: int = choice(list(self.Vessel_class))
-            self.length = self.define_length()
+            self.length: float = self.define_length()
+            self.size: int = self.define_size()
+            self.armed: bool = choice([True, False])
+            self.officers: List[str] = []
+
+            with open('known_officers.txt') as file:
+                lines = file.read().splitlines()
+                for _ in range(randint(0, 4)):
+                    self.officers += choice(lines).split(' ')
+                    self.officers.append(choice(self.__ranks__))
 
         def define_length(self) -> float:
-            length: float = 0
-            if self.vessel_class == self.Vessel_class.Corvette:
-                length = uniform(10, 150)
-            if self.vessel_class == self.Vessel_class.Frigate:
-                length = uniform(30, 250)
-            if self.vessel_class == self.Vessel_class.Cruiser:
-                length = uniform(75, 500)
-            if self.vessel_class == self.Vessel_class.Destroyer:
-                length = uniform(160, 750)
-            if self.vessel_class == self.Vessel_class.Carrier:
-                length = uniform(1000, 1200)
-            if self.vessel_class == self.Vessel_class.Dreadnought:
-                length = uniform(1000, 2500)
-            return length
+            length: int = 0
+            if self.vessel_class == 7:
+                length = randint(60, 25000)
+            if self.vessel_class == self.__Vessel_class__.Corvette:
+                length = randint(60, 280)
+            if self.vessel_class == self.__Vessel_class__.Frigate:
+                length = randint(250, 820)
+            if self.vessel_class == self.__Vessel_class__.Cruiser:
+                length = randint(75, 500)
+            if self.vessel_class == self.__Vessel_class__.Destroyer:
+                length = randint(480, 1200)
+            if self.vessel_class == self.__Vessel_class__.Carrier:
+                length = randint(1000, 4200)
+            if self.vessel_class == self.__Vessel_class__.Dreadnought:
+                length = randint(7000, 25000)
+            return float(length)
+
+        def define_size(self) -> int:
+            size: int = 0
+            if self.vessel_class == 7:
+                size = randint(3, 570)
+            if self.vessel_class == self.__Vessel_class__.Corvette:
+                size = randint(3, 8)
+            if self.vessel_class == self.__Vessel_class__.Frigate:
+                size = randint(7, 25)
+            if self.vessel_class == self.__Vessel_class__.Cruiser:
+                size = randint(12, 35)
+            if self.vessel_class == self.__Vessel_class__.Destroyer:
+                size = randint(45, 96)
+            if self.vessel_class == self.__Vessel_class__.Carrier:
+                size = randint(102, 310)
+            if self.vessel_class == self.__Vessel_class__.Dreadnought:
+                size = randint(280, 570)
+            return size
 
     def GetSpaceship(self, request, context):
-        # message = request.message
-
         spaceship = self.Spaceship()
 
         result = {'alignment': spaceship.alignment, 'name': spaceship.name, 'length': spaceship.length,
-                  'class': spaceship.vessel_class, 'size': 4, 'armed': True,
-                  'officers': ["Nick", "NeNick"]}
+                  'class': spaceship.vessel_class, 'size': spaceship.size, 'armed': spaceship.armed,
+                  'officers': spaceship.officers}
 
         return pb2.SpaceshipInfo(**result)
 
